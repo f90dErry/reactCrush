cf38bfb20df60c15467260bdedb5a7c5e888dc37
Kamasah Edwin Jnr <102263848+derrysnr@users.noreply.github.com>
2024-07-27 23:58:09 +0000

    EditPage
    worked on making the edit job button on the read more page work. .so pressing on the edit job button should take you to the edit job page in order to be able to edit the job

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import JobsPage from "./pages/JobsPage";
 import NotFoundPage  from "./pages/NotFoundPage";
 import JobPage, {jobLoader} from "./pages/JobPage";
 import AddJobPage from "./pages/AddJobPage";
+import EditJobPage from "./pages/EditJobPage";
 
 
 //Add job
@@ -27,17 +28,21 @@ const App = () => {
 
 //Delete job
 const deleteJob = async (id) => {
-  console.log("delete", id)
+   const res = await fetch(`/api/jobs/${id}`, {
+      method: 'DELETE',
+    });
+    return;
 }
 
 const router = createBrowserRouter(
   createRoutesFromElements( 
   <Route path="/" element={ <MainLayout/> }>
-     <Route index element= { <HomePage/>} />)
-     <Route path="/jobs" element= { <JobsPage />} />)
-     <Route path="/add-job" element= { <AddJobPage addJobSubmit={addJob} />} />)
-     <Route path="/jobs/:id" element= { <JobPage deleteJob={deleteJob} />} loader={jobLoader} />)
-     <Route path="*" element= { <NotFoundPage />} />)
+     <Route index element= { <HomePage/>} />
+     <Route path="/jobs" element= { <JobsPage />} />
+     <Route path="/add-job" element= { <AddJobPage addJobSubmit={addJob} />} />
+     <Route path="/edit-job/:id" element= { <EditJobPage/>}  loader={jobLoader} />
+     <Route path="/jobs/:id" element= { <JobPage deleteJob={deleteJob} />} loader={jobLoader} />
+     <Route path="*" element= { <NotFoundPage />} />
   </Route>
   )
 );
--- src/pages/EditJobPage.jsx
+++ src/pages/EditJobPage.jsx
@@ -1,46 +1,25 @@
 import React from 'react'
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
+import { useParams, useLoaderData, useNavigate } from 'react-router-dom';
 
-const AddJobPage = ({addJobSubmit}) => {
-    const [title, setTitle]= useState('');
-    const [type, setType] = useState('Full-Time');
-    const [location, setLocation] = useState('');
-    const [description, setDescription] = useState('');
-    const [salary, setSalary] = useState('Under $50K');
-    const [companyName, setCompanyName] = useState('');
-    const [companyDescription,setCompanyDescription] = useState('');
-    const [contactEmail, setContactEmail] = useState('');
-    const [contactPhone, setContactPhone] = useState('');
 
-    const navigate = useNavigate(); 
+const EditJobPage = () => {
+    const job = useLoaderData();
 
-    const submitForm = (e) => {
-        e.preventDefault();
+    const [title, setTitle]= useState(job.title);
+    const [type, setType] = useState(job.type);
+    const [location, setLocation] = useState(job.location);
+    const [description, setDescription] = useState(job.description);
+    const [salary, setSalary] = useState(job.salary);
+    const [companyName, setCompanyName] = useState(job.company.name);
+    const [companyDescription,setCompanyDescription] = useState(job.company.description);
+    const [contactEmail, setContactEmail] = useState(job.company.contactEmail);
+    const [contactPhone, setContactPhone] = useState(job.company.contactPhone);
 
-        const newJob = {
-            title,
-            type, 
-            location,
-            description,
-            salary,
-            company: {
-                name : companyName,
-                description: companyDescription,
-                contactEmail,
-                contactPhone
-            }
-        };
+    const submitForm = (e) => {};
 
-        addJobSubmit(newJob);
-
-        toast.success('Job Added successfully');
-
-        return navigate('/jobs');
-    }
   return (
-      <section className="bg-indigo-50">
+     <section className="bg-indigo-50">
       <div className="container m-auto max-w-2xl py-24">
         <div
           className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0"
@@ -222,6 +201,6 @@ const AddJobPage = ({addJobSubmit}) => {
       </div>
     </section>
   )
-};
+}
 
-export default AddJobPage;
+export default EditJobPage
